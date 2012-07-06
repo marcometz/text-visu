cdd435be158acf74fdda7783cd405f56a7a63b32
Marco Metz <marco.metz@gmail.com>
2012-07-06 12:02:02 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TextVisu::Application.config.secret_token = '0789a21506015de865cfc42a32bdc635fd39b806db8a3a9d770075a4486c6ecfdf23ce6a38a8a839ae5edc834d36402f1c164c6c4fdaff87772baa47bc679f66'
